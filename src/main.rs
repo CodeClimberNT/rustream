1d33124dee2cd339f7112406be318d79a1e23706
Nicolò Taormina <nicktaormina3@gmail.com>
2024-07-17 21:39:44 +0200

    removed mock test

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,3 @@
 fn main() {
     println!("Hello, world!");
 }
-
-// MOCK TESTS
-#[allow(dead_code)]
-fn hello_world() -> String {
-    "Hello, world!".to_string()
-}
-
-#[allow(dead_code)]
-fn mock_function() -> i32 {
-    // Mock implementation returning a specific value
-    42
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hello_world() {
-        assert_eq!(hello_world(), "Hello, world!");
-    }
-
-    #[test]
-    fn test_mock_function() {
-        // Mocking a function to return a specific value
-        let mock_result = 42;
-        let result = mock_function();
-        assert_eq!(result, mock_result);
-    }
-}
