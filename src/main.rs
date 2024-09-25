5091149683bacb2d87cb56d1eff1009f462830cd
Nicolò Taormina <nicktaormina3@gmail.com>
2024-09-25 22:50:38 +0200

    fixed blue tint

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ extern crate scrap;
 use scrap::{Capturer, Display};
 use std::thread;
 use std::time::Duration;
-// use std::fs::File;
-// use std::io::Write;
 use image::{ImageBuffer, Rgba};
 
 fn capture_screen() {
-    let display = Display::primary().expect("Couldn't find primary display.");
-    let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
+    let display: Display = Display::primary().expect("Couldn't find primary display.");
+    let mut capturer: Capturer = Capturer::new(display).expect("Couldn't begin capture.");
     let (width, height) = (capturer.width(), capturer.height());
 
     loop {
         match capturer.frame() {
             Ok(frame) => {
-                let buffer: Vec<u8> = frame.iter().cloned().collect();
+                let mut buffer: Vec<u8> = frame.iter().cloned().collect();
+                // Convert BGRA to RGBA
+                for chunk in buffer.chunks_exact_mut(4) {
+                    chunk.swap(0, 2); // Swap B and R
+                }
                 let image =
                     ImageBuffer::<Rgba<u8>, _>::from_raw(width as u32, height as u32, buffer)
                         .unwrap();
@@ -24,7 +26,7 @@ fn capture_screen() {
                 break; // Exit after capturing one frame
             }
             Err(_) => {
-                // La cattura non è riuscita, riproviamo
+                // Capture failed, retry
                 thread::sleep(Duration::from_millis(100));
             }
         }
